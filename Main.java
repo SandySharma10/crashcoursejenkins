6b6cf9ced4988d6a367565cf0532c56556232d84
SandySharma10 <138371767+SandySharma10@users.noreply.github.com>
2023-09-23 14:05:47 +0530

    added some

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("hello sandeep this is java project");
         System.out.println(new Date());
+        System.out.println("Added some comment");
     }
 
 }
